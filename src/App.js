5d3e03433d52a075e7cf407bbfcebb9ab9fbea35
Martin Marzialetti <tinchin.m@gmail.com>
2022-06-06 15:24:19 -0300

    Primera Entrega

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Creando mi nueva tienda de E-Comerce
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Martin Marzialetti
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
